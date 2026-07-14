a277d9b9e30877962f53e9778a547cbf58b41c1d
Meinanto Tri Yuriawan <45266362+meinantoyuriawan@users.noreply.github.com>
2026-07-14 19:14:23 +0700

    first commit

---
+++ src/services/vector_search_services.py
@@ -0,0 +1,10 @@
+
+
+
+class VectorSearchService:
+    def __init__(self):
+        pass
+
+    # def get_hoax_vector(self):
+
+    
---
+++ src/services/web_search_services.py
@@ -0,0 +1,12 @@
+
+
+
+class WebSearchService:
+    def __init__(self):
+        pass
+
+    # def find_from_sites
+
+    # def find_from_news
+
+    
